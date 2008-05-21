98effdfa4e5422c30801e69056208d3bc49abc8e
planet <planet@0d73084d-1af3-4f38-856e-5d6577b857c5>
2008-05-21 07:56:01 +0000

    Added mk8900image
    git-svn-id: svn+ssh://iphonelinux.org/home/linux/iphonelinux@16 0d73084d-1af3-4f38-856e-5d6577b857c5

---
+++ trunk/openiboot/mk8900image/mk8900image.c
@@ -0,0 +1,93 @@
+#include "nor_files.h"
+#include <stdio.h>
+#include <string.h>
+#include <linux/elf.h>
+
+#define BUFFERSIZE (1024*1024)
+
+char createImage(char* inElf, size_t inElfSize, char** outImage, size_t* outImageSize) {
+	Elf32_Ehdr* elf_hdr = (Elf32_Ehdr*) inElf;
+	size_t maxmem;
+
+	if(strncmp(elf_hdr->e_ident, ELFMAG, 4) != 0 ) {
+		return 0;
+	}
+
+	Elf32_Phdr* phdrs = (Elf32_Phdr*) (inElf + elf_hdr->e_phoff);
+	int i;
+
+	maxmem = 0;
+	for(i = 0; i < elf_hdr->e_phnum; i++) {
+		int memExtent = phdrs[i].p_vaddr + phdrs[i].p_memsz - elf_hdr->e_entry;
+		if(maxmem < memExtent) {
+			maxmem = memExtent;
+		}
+	}
+
+	*outImageSize = maxmem;
+	*outImage = (char*) malloc(*outImageSize);
+	memset(*outImage, 0, *outImageSize);
+	for(i = 0; i < elf_hdr->e_phnum; i++) {
+		memcpy(*outImage + phdrs[i].p_vaddr - elf_hdr->e_entry, inElf + phdrs[i].p_offset, phdrs[i].p_filesz);
+	}
+
+	return 1;
+}
+
+int main(int argc, char* argv[]) {
+	char* inElf;
+	size_t inElfSize;
+	char* outImage;
+	size_t outImageSize;
+	init_libxpwn();
+
+	if(argc < 4) {
+		printf("usage: %s <infile> <outfile> <template>\n", argv[0]);
+		return 0;
+	}
+
+	AbstractFile* template = createAbstractFileFromFile(fopen(argv[3], "rb"));
+	if(!template) {
+		fprintf(stderr, "error: cannot open template\n");
+		return 1;
+	}
+
+
+	AbstractFile* inFile = createAbstractFileFromFile(fopen(argv[1], "rb"));
+	if(!inFile) {
+		fprintf(stderr, "error: cannot open infile\n");
+		return 2;
+	}
+
+	AbstractFile* outFile = createAbstractFileFromFile(fopen(argv[2], "wb"));
+	if(!outFile) {
+		fprintf(stderr, "error: cannot open outfile\n");
+		return 3;
+	}
+
+	AbstractFile* newFile = duplicateAbstractFile(template, outFile);
+	if(!newFile) {
+		fprintf(stderr, "error: cannot duplicate file from provided template\n");
+		return 4;
+	}
+
+	inElfSize = (size_t) inFile->getLength(inFile);
+	inElf = (char*) malloc(inElfSize);
+	inFile->read(inFile, inElf, inElfSize);
+	inFile->close(inFile);
+
+	if(!createImage(inElf, inElfSize, &outImage, &outImageSize)) {
+		fprintf(stderr, "error: not a valid image\n");
+		return 5;
+	}
+
+	free(inElf);
+
+	newFile->write(newFile, outImage, outImageSize);
+	newFile->close(newFile);
+
+	free(outImage);
+
+	return 0;
+}
+
---
+++ trunk/openiboot/mk8900image/nor_files.h
@@ -0,0 +1,19 @@
+#ifndef NOR_FILES_H
+#define NOR_FILES_H
+
+#include "common.h"
+#include "8900.h"
+#include "img2.h"
+#include "lzssfile.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	AbstractFile* openAbstractFile(AbstractFile* file);
+	AbstractFile* duplicateAbstractFile(AbstractFile* file, AbstractFile* backing);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
