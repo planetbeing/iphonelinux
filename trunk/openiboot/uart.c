a9221c3265dae747605697d125050215a35ae1bf
planet <planet@0d73084d-1af3-4f38-856e-5d6577b857c5>
2008-05-23 21:58:13 +0000

    Started uart
    git-svn-id: svn+ssh://iphonelinux.org/home/linux/iphonelinux@34 0d73084d-1af3-4f38-856e-5d6577b857c5

---
+++ trunk/openiboot/uart.c
@@ -0,0 +1,9 @@
+#include "openiboot.h"
+#include "uart.h"
+#include "s5l8900.h"
+
+int uart_setup() {
+
+	return 0;
+}
+
---
+++ trunk/openiboot/uart.h
@@ -0,0 +1,8 @@
+#ifndef UART_H
+#define UART_H
+
+#include "openiboot.h"
+
+int uart_setup();
+
+#endif
