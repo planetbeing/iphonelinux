71bc892b922bf2a2f8ff3c82ae9adb77bbbf9db5
cmw <cmw@0d73084d-1af3-4f38-856e-5d6577b857c5>
2008-05-24 17:19:04 +0000

    Add some uart code
    git-svn-id: svn+ssh://iphonelinux.org/home/linux/iphonelinux@43 0d73084d-1af3-4f38-856e-5d6577b857c5

--- trunk/openiboot/uart.c
+++ trunk/openiboot/uart.c
@@ -7,3 +7,28 @@ int uart_setup() {
 	return 0;
 }
 
+
+int uart_set_clk(int ureg, int mode) {
+
+	return 0;
+
+}
+int uart_set_sample_rate(int ureg, int rate) {
+
+	return 0;
+}
+int uart_set_flow_control(int ureg, int mode) {
+	
+	return 0;
+}
+int uart_set_mode(int ureg, int mode) {
+
+	return 0;
+}
+int uart_write(int ureg, char *buffer) {
+
+
+	if(ureg > 4)
+		return -1; // Invalid ureg
+
+}
--- trunk/openiboot/uart.h
+++ trunk/openiboot/uart.h
@@ -3,6 +3,17 @@
 
 #include "openiboot.h"
 
+/* UART Control Registers */
+#define UCON0  (*(volatile unsigned short *)0x3CC00004)
+#define UCON1  (*(volatile unsigned short *)0x3CC04004)
+#define UCON2	 (*(volatile unsigned short *)0x3CC08004)
+#define UCON3	 (*(volatile unsigned short *)0x3CC0C004)
+#define UCON4  (*(volatile unsigned short *)0x3CC10000)
+
 int uart_setup();
+int uart_set_clk(int ureg, int mode);
+int uart_set_sample_rate(int ureg, int rate);
+int uart_set_flow_control(int ureg, int mode);
+int uart_set_mode(int ureg, int mode);
 
 #endif
