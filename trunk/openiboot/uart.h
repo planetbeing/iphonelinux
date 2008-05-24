7115230d63717ebfab566cd0f1bb91421fc8a55d
cmw <cmw@0d73084d-1af3-4f38-856e-5d6577b857c5>
2008-05-24 17:20:05 +0000

    bad control reg
    git-svn-id: svn+ssh://iphonelinux.org/home/linux/iphonelinux@44 0d73084d-1af3-4f38-856e-5d6577b857c5

--- trunk/openiboot/uart.h
+++ trunk/openiboot/uart.h
@@ -8,7 +8,7 @@
 #define UCON1  (*(volatile unsigned short *)0x3CC04004)
 #define UCON2	 (*(volatile unsigned short *)0x3CC08004)
 #define UCON3	 (*(volatile unsigned short *)0x3CC0C004)
-#define UCON4  (*(volatile unsigned short *)0x3CC10000)
+#define UCON4  (*(volatile unsigned short *)0x3CC10004)
 
 int uart_setup();
 int uart_set_clk(int ureg, int mode);
