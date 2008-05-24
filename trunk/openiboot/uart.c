d30e03b87adec6cf538e9164e03cfd2d2907d805
cmw <cmw@0d73084d-1af3-4f38-856e-5d6577b857c5>
2008-05-24 17:59:31 +0000

    More uart_init stuff
    git-svn-id: svn+ssh://iphonelinux.org/home/linux/iphonelinux@47 0d73084d-1af3-4f38-856e-5d6577b857c5

--- trunk/openiboot/uart.c
+++ trunk/openiboot/uart.c
@@ -4,20 +4,51 @@
 
 int uart_setup() {
 	
+	uart_settings u_set[5];
+
 	// Set word size to 8bits
 	SET_REG(UCON4, 0x01)
 	SET_REG(UCON3, 0x01)
 	SET_REG(UCON2, 0x01)
 	SET_REG(UCON1, 0x01)
 	SET_REG(UCON0, 0x01)
-
 	// Set parity to even
   SET_REG(UCON4+4, 0x05)
   SET_REG(UCON3+4, 0x05)
   SET_REG(UCON2+4, 0x05)
   SET_REG(UCON1+4, 0x05)
   SET_REG(UCON0+4, 0x05)
-	
+	// Set ureg values
+	u_set[0].ureg = 0x00;
+	u_set[1].ureg = 0x01;
+	u_set[2].ureg = 0x02;
+	u_set[3].ureg = 0x03;
+	u_set[4].ureg = 0x04;
+	// Set baud rate	
+	u_set[0].ureg = BAUD_115200;
+	u_set[1].ureg = BAUD_115200;
+	u_set[2].ureg = BAUD_115200;
+	u_set[3].ureg = BAUD_115200;
+	u_set[4].ureg = BAUD_115200;
+	// Set clock
+	uart_set_clk(0x00, 0x01);
+	uart_set_clk(0x01, 0x01);
+	uart_set_clk(0x02, 0x01);
+	uart_set_clk(0x03, 0x01);
+	uart_set_clk(0x04, 0x01);
+	// Set sample rates
+  uart_set_sample_rate(0x00, 0x10);
+	uart_set_sample_rate(0x01, 0x10);
+	uart_set_sample_rate(0x02, 0x10);
+	uart_set_sample_rate(0x03, 0x10);
+	uart_set_sample_rate(0x04, 0x10);
+	// Set flow control 
+	uart_set_flow_control(0x00, FLOW_CONTROL_OFF);
+	uart_set_flow_control(0x01, FLOW_CONTROL_ON);
+	uart_set_flow_control(0x02, FLOW_CONTROL_ON);
+	uart_set_flow_control(0x03, FLOW_CONTROL_ON);
+	uart_set_flow_control(0x04, FLOW_CONTROL_OFF);
+
 	return 0;
 }
 
