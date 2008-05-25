2ffa5f92be79c9a7ae3923c39c20259c007d8bbd
planet <planet@0d73084d-1af3-4f38-856e-5d6577b857c5>
2008-05-25 09:51:06 +0000

    done with uart helper functions
    git-svn-id: svn+ssh://iphonelinux.org/home/linux/iphonelinux@60 0d73084d-1af3-4f38-856e-5d6577b857c5

--- trunk/openiboot/includes/uart.h
+++ trunk/openiboot/includes/uart.h
@@ -7,19 +7,35 @@
 #define FLOW_CONTROL_ON	 0x01
 #define FLOW_CONTROL_OFF 0x00
 
-typedef struct uart_settings {
+typedef struct UARTSettings {
 	uint32_t ureg;
 	uint32_t baud;
 	uint32_t sample_rate;
-	uint32_t flow_control;
+	OnOff flow_control;
 	uint32_t mode;
-	uint32_t divider;
-} uart_settings;
+	uint32_t clock;
+} UARTSettings;
+
+typedef struct UARTRegisters {
+	uint32_t ULCON;
+	uint32_t UCON;
+	uint32_t UFCON;
+	uint32_t UMCON;
+	uint32_t UTRSTAT;
+	uint32_t UERSTAT;
+	uint32_t UFSTAT;
+	uint32_t UMSTAT;
+	uint32_t UTXH2;
+	uint32_t URXH2;
+	uint32_t UBAUD;
+	uint32_t UINTP;
+} UARTRegisters;
 
 int uart_setup();
-int uart_set_clk(int ureg, int mode);
+int uart_set_clk(int ureg, int clock);
 int uart_set_sample_rate(int ureg, int rate);
-int uart_set_flow_control(int ureg, int mode);
-int uart_set_mode(int ureg, int mode);
+int uart_set_flow_control(int ureg, OnOff flow_control);
+int uart_set_mode(int ureg, uint32_t mode);
+int uart_set_baud_rate(int ureg, uint32_t baud);
 
 #endif
--- trunk/openiboot/uart.c
+++ trunk/openiboot/uart.c
@@ -1,11 +1,12 @@
 #include "openiboot.h"
 #include "uart.h"
+#include "clock.h"
 #include "hardware/uart.h"
 
-int uart_setup() {
-	
-	uart_settings u_set[5];
+UARTRegisters HWUarts[5];
+UARTSettings UARTs[5];
 
+int uart_setup() {
 	// Set word size to 8bits
 	SET_REG(UCON4, 0x01);
 	SET_REG(UCON3, 0x01);
@@ -13,23 +14,23 @@ int uart_setup() {
 	SET_REG(UCON1, 0x01);
 	SET_REG(UCON0, 0x01);
 	// Set parity to even
-  SET_REG(UCON4+4, 0x05);
-  SET_REG(UCON3+4, 0x05);
-  SET_REG(UCON2+4, 0x05);
-  SET_REG(UCON1+4, 0x05);
-  SET_REG(UCON0+4, 0x05);
+	SET_REG(UCON4+4, 0x05);
+	SET_REG(UCON3+4, 0x05);
+	SET_REG(UCON2+4, 0x05);
+	SET_REG(UCON1+4, 0x05);
+	SET_REG(UCON0+4, 0x05);
 	// Set ureg values
-	u_set[0].ureg = 0x00;
-	u_set[1].ureg = 0x01;
-	u_set[2].ureg = 0x02;
-	u_set[3].ureg = 0x03;
-	u_set[4].ureg = 0x04;
+	UARTs[0].ureg = 0x00;
+	UARTs[1].ureg = 0x01;
+	UARTs[2].ureg = 0x02;
+	UARTs[3].ureg = 0x03;
+	UARTs[4].ureg = 0x04;
 	// Set baud rate	
-	u_set[0].ureg = BAUD_115200;
-	u_set[1].ureg = BAUD_115200;
-	u_set[2].ureg = BAUD_115200;
-	u_set[3].ureg = BAUD_115200;
-	u_set[4].ureg = BAUD_115200;
+	UARTs[0].ureg = BAUD_115200;
+	UARTs[1].ureg = BAUD_115200;
+	UARTs[2].ureg = BAUD_115200;
+	UARTs[3].ureg = BAUD_115200;
+	UARTs[4].ureg = BAUD_115200;
 	// Set clock
 	uart_set_clk(0x00, 0x01);
 	uart_set_clk(0x01, 0x01);
@@ -37,7 +38,7 @@ int uart_setup() {
 	uart_set_clk(0x03, 0x01);
 	uart_set_clk(0x04, 0x01);
 	// Set sample rates
-  uart_set_sample_rate(0x00, 0x10);
+	uart_set_sample_rate(0x00, 0x10);
 	uart_set_sample_rate(0x01, 0x10);
 	uart_set_sample_rate(0x02, 0x10);
 	uart_set_sample_rate(0x03, 0x10);
@@ -53,23 +54,105 @@ int uart_setup() {
 }
 
 
-int uart_set_clk(int ureg, int mode) {
+int uart_set_clk(int ureg, int clock) {
+	if(ureg > 4)
+		return -1; // Invalid ureg
+
+	if(clock != UART_CLOCK_PCLK && clock != UART_CLOCK_EXT_UCLK0 && clock != UART_CLOCK_EXT_UCLK1) {
+		return -1; // Invalid clock		
+	}
+
+	SET_REG(HWUarts[ureg].UCON,
+		GET_REG(HWUarts[ureg].UCON) & (~UART_CLOCK_SELECTION_MASK) | (clock << UART_CLOCK_SELECTION_SHIFT));
+
+	UARTs[ureg].clock = clock;
+	uart_set_baud_rate(ureg, UARTs[ureg].baud);
 
 	return 0;
-
 }
+
+int uart_set_baud_rate(int ureg, uint32_t baud) {
+	if(ureg > 4)
+		return -1; // Invalid ureg
+
+	uint32_t clockFrequency = (UARTs[ureg].clock == 0) ? PeripheralFrequency : FixedFrequency;
+	uint32_t div_val = clockFrequency / (baud * UARTs[ureg].sample_rate) - 1;
+
+	SET_REG(HWUarts[ureg].UBAUD, GET_REG(HWUarts[ureg].UBAUD) & (~UART_DIVVAL_MASK) | div_val);
+
+	// vanilla iBoot also does a reverse calculation from div_val and solves for baud and reports
+	// the "actual" baud rate, or what is after loss during integer division
+
+	UARTs[ureg].baud = baud;
+
+	return 0;
+}
+
 int uart_set_sample_rate(int ureg, int rate) {
+	if(ureg > 4)
+		return -1; // Invalid ureg
+
+	uint32_t newSampleRate;
+	switch(rate) {
+		case 4:
+			newSampleRate = UART_SAMPLERATE_4;
+			break;
+		case 8:
+			newSampleRate = UART_SAMPLERATE_8;
+			break;
+		case 16:
+			newSampleRate = UART_SAMPLERATE_16;
+			break;
+		default:
+			return -1; // Invalid sample rate
+	}
+
+	SET_REG(HWUarts[ureg].UBAUD,
+		GET_REG(HWUarts[ureg].UBAUD) & (~UART_SAMPLERATE_MASK) | (newSampleRate << UART_SAMPLERATE_SHIFT));
+
+	UARTs[ureg].clock = rate;
+	uart_set_baud_rate(ureg, UARTs[ureg].baud);
 
 	return 0;
 }
-int uart_set_flow_control(int ureg, int mode) {
-	
-	return 0;
+
+int uart_set_flow_control(int ureg, OnOff flow_control) {
+	if(ureg > 4)
+		return -1; // Invalid ureg
+
+	if(ureg == 0)
+		return -1; // uart0 does not support flow control
+
+	if(flow_control == ON) {
+		SET_REG(HWUarts[ureg].UMCON, UART_UMCON_AFC_BIT);
+	} else {
+		SET_REG(HWUarts[ureg].UMCON, UART_UMCON_NRTS_BIT);
+	}
+
+	UARTs[ureg].flow_control = flow_control;
 }
-int uart_set_mode(int ureg, int mode) {
+
+int uart_set_mode(int ureg, uint32_t mode) {
+	if(ureg > 4)
+		return -1; // Invalid ureg
+
+	UARTs[ureg].mode = mode;
+
+	if(mode == 0) {
+		// Setup some defaults, like no loopback mode
+		SET_REG(HWUarts[ureg].UCON, 
+			GET_REG(HWUarts[ureg].UCON) & (~UART_UCON_UNKMASK) & (~UART_UCON_UNKMASK) & (~UART_UCON_LOOPBACKMODE));
+
+		// Use polling mode
+		SET_REG(HWUarts[ureg].UCON, 
+			GET_REG(HWUarts[ureg].UCON) & (~UART_UCON_RXMODE_MASK) & (~UART_UCON_TXMODE_MASK)
+			| (UART_UCON_MODE_IRQORPOLL << UART_UCON_RXMODE_SHIFT)
+			| (UART_UCON_MODE_IRQORPOLL << UART_UCON_TXMODE_SHIFT));
+	}
 
 	return 0;
 }
+
 int uart_write(int ureg, char *buffer) {
 
 	if(ureg > 4)
