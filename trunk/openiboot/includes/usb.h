e947905aa38b981a8df4a33cb2176c0d65ee2d1e
planet <planet@0d73084d-1af3-4f38-856e-5d6577b857c5>
2008-05-26 18:55:35 +0000

    Added usb state enum
    git-svn-id: svn+ssh://iphonelinux.org/home/linux/iphonelinux@70 0d73084d-1af3-4f38-856e-5d6577b857c5

--- trunk/openiboot/includes/usb.h
+++ trunk/openiboot/includes/usb.h
@@ -1,6 +1,14 @@
 #ifndef USB_H
 #define USB_H
 
+typedef enum USBState {
+	USBStart = 0,
+	USBPowered = 1,
+	USBDefault = 2,
+	USBAddress = 3,
+	USBConfigured = 4
+};
+
 int usb_setup();
 int usb_shutdown();
 
