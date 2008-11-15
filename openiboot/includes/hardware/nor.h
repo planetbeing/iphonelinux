040ad0ce0082f0da14f38dd8c8acdb000727e450
planetbeing <planetbeing@gmail.com>
2008-11-15 16:31:56 -0500

    Preliminary SPI NOR support for 3G

--- openiboot/includes/hardware/nor.h
+++ openiboot/includes/hardware/nor.h
@@ -23,6 +23,28 @@
 #define ERASE_DATA 0x3030
 #define LOCK_UNLOCK 0x5555
 
+#define NOR_SPI_READ 0x0B
+#define NOR_SPI_WREN 0x06
+#define NOR_SPI_WRDI 0x04
+#define NOR_SPI_PRGM 0x02
+#define NOR_SPI_RDSR 0x05
+#define NOR_SPI_WRSR 0x01
+#define NOR_SPI_EWSR 0x50
+#define NOR_SPI_ERSE_4KB 0x20
+#define NOR_SPI_JEDECID 0x9F
+
+#define NOR_SPI_SR_BUSY 0
+#define NOR_SPI_SR_WEL 1
+#define NOR_SPI_SR_BP0 2
+#define NOR_SPI_SR_BP1 3
+#define NOR_SPI_SR_BP2 4
+#define NOR_SPI_SR_BP3 5
+#define NOR_SPI_SR_AAI 6
+#define NOR_SPI_SR_BPL 7
+
+#define NOR_T_BP 10
+#define NOR_T_SE 25
+
 #define NOR_CLOCKGATE 0x1E
 
 #endif
--- openiboot/nor.c
+++ openiboot/nor.c
@@ -4,9 +4,52 @@
 #include "util.h"
 #include "timer.h"
 
+#ifdef CONFIG_3G
+#include "spi.h"
+#include "gpio.h"
+#include "hardware/spi.h"
+#endif
+
 static int NORSectorSize = 4096;
 
+static int Prepared = 0;
+
+static void nor_prepare() {
+#ifdef CONFIG_3G
+	if(Prepared == 0)
+		spi_set_baud(0, 12000000, SPIOption13Setting0, 1, 0, 0);
+#endif
+	Prepared++;
+}
+
+static void nor_unprepare() {
+	Prepared--;
+}
+
 static NorInfo* probeNOR() {
+	nor_prepare();
+
+#ifdef CONFIG_3G
+	uint8_t command = NOR_SPI_JEDECID;
+	uint8_t deviceID[3];
+	gpio_pin_output(GPIO_SPI0_CS0, 0);
+	spi_tx(0, &command, 1, TRUE, 0);
+	spi_rx(0, deviceID, 3, TRUE, 0);
+	gpio_pin_output(GPIO_SPI0_CS0, 1);
+	uint16_t vendor = deviceID[0];
+	uint16_t device = deviceID[3];
+
+	// Unprotect NOR
+	command = NOR_SPI_EWSR;
+	gpio_pin_output(GPIO_SPI0_CS0, 0);
+	spi_tx(0, &command, 1, TRUE, 0);
+	gpio_pin_output(GPIO_SPI0_CS0, 1);
+
+	uint8_t wrsrCommand[2] = {NOR_SPI_WRSR, 0};
+	gpio_pin_output(GPIO_SPI0_CS0, 0);
+	spi_tx(0, wrsrCommand, 2, TRUE, 0);
+	gpio_pin_output(GPIO_SPI0_CS0, 1);
+#else
 	SET_REG16(NOR + COMMAND, COMMAND_UNLOCK);
 	SET_REG16(NOR + LOCK, LOCK_UNLOCK);
 
@@ -20,14 +63,71 @@ static NorInfo* probeNOR() {
 	SET_REG16(NOR, DATA_MODE);
 	GET_REG16(NOR);
 	GET_REG16(NOR);
+#endif
 
 	bufferPrintf("NOR vendor=%x, device=%x\r\n", (uint32_t) vendor, (uint32_t) device);
+
+	nor_unprepare();
 
 	return NULL;
 }
 
-void nor_write_word(uint32_t offset, uint16_t data) {
+#ifdef CONFIG_3G
+static void nor_write_byte(uint32_t offset, uint8_t data) {
+	nor_prepare();
 
+	uint8_t command[5];
+	command[0] = NOR_SPI_PRGM;
+	command[1] = (offset >> 16) && 0xFF;
+	command[2] = (offset >> 8) && 0xFF;
+	command[3] = offset && 0xFF;
+	command[4] = data;
+
+	gpio_pin_output(GPIO_SPI0_CS0, 0);
+	spi_tx(0, command, sizeof(command), TRUE, 0);
+	gpio_pin_output(GPIO_SPI0_CS0, 1);
+	udelay(NOR_T_BP);
+
+	nor_unprepare();
+}
+
+static void nor_write_enable() {
+	nor_prepare();
+
+	uint8_t command[5];
+	command[0] = NOR_SPI_WREN;
+
+	gpio_pin_output(GPIO_SPI0_CS0, 0);
+	spi_tx(0, command, sizeof(command), TRUE, 0);
+	gpio_pin_output(GPIO_SPI0_CS0, 1);
+
+	nor_unprepare();
+}
+
+static void nor_write_disable() {
+	nor_prepare();
+
+	uint8_t command[5];
+	command[0] = NOR_SPI_WRDI;
+
+	gpio_pin_output(GPIO_SPI0_CS0, 0);
+	spi_tx(0, command, sizeof(command), TRUE, 0);
+	gpio_pin_output(GPIO_SPI0_CS0, 1);
+
+	nor_unprepare();
+
+}
+#endif
+
+void nor_write_word(uint32_t offset, uint16_t data) {
+	nor_prepare();
+
+#ifdef CONFIG_3G
+	nor_write_enable();
+	nor_write_byte(offset, data & 0xFF);
+	nor_write_byte(offset + 1, (data >> 8) & 0xFF); 
+	nor_write_disable();
+#else
 	SET_REG16(NOR + COMMAND, COMMAND_UNLOCK);
 	SET_REG16(NOR + LOCK, LOCK_UNLOCK);
 
@@ -42,13 +142,57 @@ void nor_write_word(uint32_t offset, uint16_t data) {
 			break;
 		}
 	}
+#endif
+
+	nor_unprepare();
 }
 
 uint16_t nor_read_word(uint32_t offset) {
-	return GET_REG16(NOR + offset);
+	uint16_t data;
+	nor_prepare();
+
+#ifdef CONFIG_3G
+	uint8_t command[5];
+	command[0] = NOR_SPI_READ;
+	command[1] = (offset >> 16) && 0xFF;
+	command[2] = (offset >> 8) && 0xFF;
+	command[3] = offset && 0xFF;
+	command[4] = 0;
+
+	gpio_pin_output(GPIO_SPI0_CS0, 0);
+	spi_tx(0, command, sizeof(command), TRUE, 0);
+	spi_rx(0, (uint8_t*) &data, sizeof(data), TRUE, 0);
+	gpio_pin_output(GPIO_SPI0_CS0, 1);
+
+	nor_unprepare();
+#else
+	data = GET_REG16(NOR + offset);
+#endif
+	nor_unprepare();
+
+	return data;
 }
 
 void nor_erase_sector(uint32_t offset) {
+	nor_prepare();
+
+#ifdef CONFIG_3G
+	nor_write_enable();
+
+	uint8_t command[3];
+	command[0] = NOR_SPI_ERSE_4KB;
+	command[1] = (offset >> 16) && 0xFF;
+	command[2] = (offset >> 8) && 0xFF;
+	command[3] = offset && 0xFF;
+
+	gpio_pin_output(GPIO_SPI0_CS0, 0);
+	spi_tx(0, command, sizeof(command), TRUE, 0);
+	gpio_pin_output(GPIO_SPI0_CS0, 1);
+
+	udelay(NOR_T_SE);
+
+	nor_write_disable();
+#else
 	SET_REG16(NOR + COMMAND, COMMAND_UNLOCK);
 	SET_REG16(NOR + LOCK, LOCK_UNLOCK);
 
@@ -67,9 +211,14 @@ void nor_erase_sector(uint32_t offset) {
 			break;
 		}
 	}
+#endif
+
+	nor_unprepare();
 }
 
 void nor_read(void* buffer, int offset, int len) {
+	nor_prepare();
+
 	uint16_t* alignedBuffer = (uint16_t*) buffer;
 	for(; len >= 2; len -= 2) {
 		*alignedBuffer = nor_read_word(offset);
@@ -82,9 +231,13 @@ void nor_read(void* buffer, int offset, int len) {
 		uint8_t* unalignedBuffer = (uint8_t*) alignedBuffer;
 		*unalignedBuffer = *((uint8_t*)(&lastWord));
 	}
+
+	nor_unprepare();
 }
 
 void nor_write(void* buffer, int offset, int len) {
+	nor_prepare();
+
 	int startSector = offset / NORSectorSize;
 	int endSector = (offset + len) / NORSectorSize;
 
@@ -107,6 +260,8 @@ void nor_write(void* buffer, int offset, int len) {
 	}
 
 	free(sectorsToChange);
+
+	nor_unprepare();
 }
 
 int getNORSectorSize() {
